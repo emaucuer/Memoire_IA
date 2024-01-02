547d367a68dd7ac38b513223d76e8c9e25769526
Eurydice MAUCUER <e.maucuer@gmail.com>
2024-01-02 17:34:06 +0100

    Move files and work on the single data file

--- Code/03-Creation_data_model/06_prep_data_model.R
+++ Code/03-Creation_data_model/06_prep_data_model.R
@@ -20,33 +20,34 @@ library(sf)
 #------------------------------------------------------------------------------#
 # Récupération des données -----------------------------------------------------
 #------------------------------------------------------------------------------#
-dataFinale <- read_rds("../../Data/indic_aladin_1982_2021_all_V2.rds")
-climsecA1B <- read_rds("../../Data/indic_climsec_2000_2050_A1B.rds")
-climsecA2 <- read_rds("../../Data/indic_climsec_2000_2050_A2.rds")
-climsecB1 <- read_rds("../../Data/indic_climsec_2000_2050_B1.rds")
-climsecHist <- read_rds("../../Data/indic_climsec_1982_1999.rds")
+dataAladin <- read_rds("../../Data/DRIAS_Aladin/lissage/indic_aladin_1982_2021_all_V2.rds")
+climsecA1B <- read_rds("../../Data/DRIAS_Climsec/lissage/indic_climsec_2000_2050_A1B.rds")
+climsecA2 <- read_rds("../../Data/DRIAS_Climsec/lissage/indic_climsec_2000_2050_A2.rds")
+climsecB1 <- read_rds("../../Data/DRIAS_Climsec/lissage/indic_climsec_2000_2050_B1.rds")
+climsecHist <- read_rds("../../Data/DRIAS_Climsec/lissage/indic_climsec_1982_1999.rds")
 new_sswi <- read_rds("../../Data/sswi/final_sswi_1982_2021.rds")
-
+gaspar_ag_an <- read_rds("../../Data/Gaspar/gaspar_ag.rds")
 
 #------------------------------------------------------------------------------#
 # Ajout des CatNat GASPAR ------------------------------------------------------
 #------------------------------------------------------------------------------#
-## Load data gaspar et aggrégation annuelle ------------------------------------
-gaspar_an <- read_rds("../../Data/Gaspar/gaspar_ag.rds")
+setDT(dataAladin)
+setDT(gaspar_ag_an)
+dataFinale <- merge(dataAladin, gaspar_ag_an, 
+                    by.x=c("id", "annee"), 
+                    by.y=c("cod_commune", "annee"),
+                    all.x=T) 
 
-
-## Ajout des infos gaspar dans la base finale ----------------------------------
-setDT(dataFinale)
-setDT(gaspar_an)
-dataFinale <- merge(dataFinale, gaspar_an, 
-                     by.x=c("id", "annee"), by.y=c("cod_commune", "an_debut"), # remplacer par annee pour pluri annuel
-                     all.x=T) 
 dataFinale <- dataFinale %>%  
   mutate(inondation = replace_na(inondation,0),
-         secheresse = ifelse(annee>=1988, replace_na(secheresse,0), NA))
+         inondation_pl = replace_na(inondation_pl,0),
+         secheresse = ifelse(annee>=1988, replace_na(secheresse,0), NA),
+         secheresse_pl = ifelse(annee>=1988, replace_na(secheresse_pl,0), NA))
 
 
 ## Historique 3 ans ------------------------------------------------------------
+list_var <- c("id", "annee", "inondation", "secheresse", "inondation_pl", "secheresse_pl")
+
 catnat3ans <- dataFinale %>%
   left_join(dataFinale %>% mutate(annee=annee+1) %>% select(id, annee, inondation, secheresse), 
             by = c("id", "annee"), suffix = c("", "1")) %>%
@@ -61,9 +62,10 @@ catnat3ans <- dataFinale %>%
 
 dataFinale <- dataFinale %>% merge(catnat3ans, by = c("id", "annee"))
 
-
+#------------------------------------------------------------------------------#
 # Ajout des altitudes + departement --------------------------------------------
-infos_com <- readRDS("../Data/infos_communes.rds")
+#------------------------------------------------------------------------------#
+infos_com <- readRDS("../../Data/Administratif/infos_communes.rds")
 
 dataFinale <- dataFinale %>%
   merge(infos_com %>% select(code_insee, commune, altitude, code_departement) ,
@@ -76,9 +78,11 @@ table(is.na(dataFinale$dep)) #aucun departement manquant
 
 
 
+#------------------------------------------------------------------------------#
 # Ajout des expositions aux risques (RGA et PPRI) ------------------------------
-rga2019Finale <- read_rds("../Data/CATNAT/rga2019.rds")
-ppriCom <- read_rds("../Data/CATNAT/ppriCom.rds")
+#------------------------------------------------------------------------------#
+rga2019Finale <- read_rds("../../Data/RGA/rga2019.rds")
+ppriCom <- read_rds("../../Data/Gaspar/ppriCom.rds")
 
 rga2019Finale <- rga2019Finale %>% 
   select(code_insee, part_alea_faible_commune, part_alea_moyen_fort_commune,
@@ -91,8 +95,11 @@ dataFinale <- dataFinale %>%
   relocate(c(commune, code_dep, inondation, secheresse), .after=id)
 
 
-
+#------------------------------------------------------------------------------#
 # Ajout des indicateurs de secheresse ------------------------------------------
+#------------------------------------------------------------------------------#
+
+## Donnees CLIMSEC
 climsecA1B <- bind_rows(climsecA1B, climsecHist) %>% 
   rename(spi_A1B = spi, sswi_A1B = sswi) %>%
   select(-longM, -latM)
@@ -103,7 +110,6 @@ climsecB1 <- bind_rows(climsecB1, climsecHist) %>%
   rename(spi_B1 = spi, sswi_B1 = sswi) %>%
   select(-longM, -latM)
 
-
 setDT(climsecA1B)
 setDT(climsecB1)
 setDT(climsecA2)
@@ -112,18 +118,15 @@ climsec_all <- climsecA1B %>%
   merge(climsecB1, by = c("id", "annee")) %>%
   merge(climsecA2 , by = c("id", "annee"))
 
-
 dataFinaleClimsec <- merge(dataFinale, climsec_all, by = c("id", "annee"), all.x=T)
-dataFinale_sswi <- merge(dataFinale, new_sswi %>% select(-longM,-latM), 
+
+
+## Donnees Meteo France retraitees
+dataFinale_sswi <- merge(dataFinaleClimsec, new_sswi %>% select(-longM,-latM), 
                          by = c("id", "annee"), all.x=T)
 
 
 # Enregistrement ---------------------------------------------------------------
-write_rds(dataFinale, "../Data/modelisation/data_modelisation_1982_2021_all_V2.rds")
-# write_rds(dataFinaleA1B, "../Data/modelisation/data_modelisation_1982_2021_all_V2_A1B.rds")
-# write_rds(dataFinaleB1, "../Data/modelisation/data_modelisation_1982_2021_all_V2_B1.rds")
-# write_rds(dataFinaleA2, "../Data/modelisation/data_modelisation_1982_2021_all_V2_A2.rds")
-write_rds(dataFinaleClimsec, "../Data/modelisation/data_modelisation_1982_2021_all_v2_climsec.rds")
 write_rds(dataFinale_sswi, "../Data/modelisation/data_modelisation_1982_2021_f.rds")
 
 
